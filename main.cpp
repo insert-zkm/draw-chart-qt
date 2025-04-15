43f4b5fc37964deb76776ba5be977bc21ae9ad45
insert-zkm <zkanylbaev03@gmail.com>
2025-04-15 23:26:18 +0700

    delete test code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include <QDebug>
+#include "src/extractdata.h";
+
+#include <QSqlDatabase>
+#include <QSqlRecord>
+#include <QSqlQuery>
+#include <QSqlError>
+
+#include <QFileInfo>
+
 
 int main() {
+    IExtractData* st = new SqlExtract();
+    QFileInfo f("../draw-chart-qt/test/data/TEMPERATURE_NOVOSIB.sqlite");
+
+    TimeSeries a = st->exec(f);
+
+    for(auto it = a.begin(); it != a.end(); it++) {
+        qDebug() << (*it);
+    }
 
     return 0;
 }
